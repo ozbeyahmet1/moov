dc467dbd6b2662299cf682a309d4e8d0c1bfa465
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-19 03:51:39 +0300

    feat: creates data fetching system and helper tools

---
+++ src/interfaces/movie.ts
@@ -0,0 +1,7 @@
+export interface Movie {
+  readonly id: number;
+  readonly title: string;
+  readonly overview: string;
+  readonly poster_path: string;
+  readonly release_date: string;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,76 @@
-import { Inter } from 'next/font/google';
+import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import { Category } from '../interfaces/category';
+import { Movie } from '../interfaces/movie';
+import {
+  categoriesEndpoint,
+  getCategoriesData,
+  getMoviesData,
+  movieEndpoints,
+} from '../utils';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+interface MoviesPageProps {
+  readonly trendingMovies: ReadonlyArray<Movie>;
+  readonly topRatedMovies: ReadonlyArray<Movie>;
+  readonly netflixOriginals: ReadonlyArray<Movie>;
+  readonly categories: ReadonlyArray<Category>;
 }
+
+export const getStaticProps: GetStaticProps<MoviesPageProps> = async () => {
+  try {
+    const [trendingMovies, topRatedMovies, netflixOriginals, categories] =
+      await Promise.all([
+        getMoviesData(movieEndpoints.trending),
+        getMoviesData(movieEndpoints.trending),
+        getMoviesData(movieEndpoints.netflixOriginals),
+        getCategoriesData(categoriesEndpoint),
+      ]);
+    return {
+      props: {
+        trendingMovies,
+        topRatedMovies,
+        netflixOriginals,
+        categories,
+      },
+      revalidate: 60 * 60 * 24, // Revalidate every 24 hours
+    };
+  } catch (error) {
+    console.error('Failed to fetch movie data:', error);
+    return {
+      notFound: true,
+    };
+  }
+};
+
+const MoviesPage = ({ trendingMovies, categories }: MoviesPageProps) => {
+  return (
+    <div>
+      <h1>Popular Movies</h1>
+      <div className="grid grid-cols-4">
+        {trendingMovies?.map((movie) => (
+          <div key={movie.id}>
+            {/* <h2>{movie.title}</h2>
+          <p>{movie.overview}</p> */}
+            <Image
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={`${movie.title} Poster`}
+              width={320}
+              height={400}
+            />
+          </div>
+        ))}
+      </div>
+      <div>
+        {categories.map((category) => {
+          return (
+            <h3 className="bg-red-500" key={category.id}>
+              {category.name}
+            </h3>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MoviesPage;
