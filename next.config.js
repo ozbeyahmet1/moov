31ab03fd7bc6047d2c19570a19b0b1aafc05073a
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-19 03:48:26 +0300

    feat: adds 'api.themoviedb.org', 'image.tmdb.org' and api key to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_TMDB_API_KEY: process.env.NEXT_TMDB_API_KEY,
+  },
+  images: {
+    domains: ['api.themoviedb.org', 'image.tmdb.org'],
+  },
 };
 
 module.exports = nextConfig;
