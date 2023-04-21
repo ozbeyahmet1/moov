3c48126f8fb04a09f2fb82e797019d064c424259
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-21 05:48:39 +0300

    feat: creates header component

---
+++ src/components/layout/logo/logo.tsx
@@ -0,0 +1,13 @@
+import { Tilt_Prism } from 'next/font/google';
+
+const tiltPrism = Tilt_Prism({ subsets: ['latin'] });
+
+export const Logo = () => {
+  return (
+    <h1
+      className={`${tiltPrism.className} text-5xl font-bold uppercase text-white`}
+    >
+      moov
+    </h1>
+  );
+};
