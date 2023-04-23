976c69e1467f329e921be1a4c261bc29d683ff04
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-23 05:00:13 +0300

    feat: creates goToPage component

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -1,4 +1,5 @@
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 interface Props {
   readonly element_number: number;
@@ -8,17 +9,12 @@ interface Props {
 }
 
 const twClasses = {
-  paginationElement:
-    'rounded-sm border-2 border-white bg-transparent px-2 text-white cursor-pointer',
-  currentPaginationElement:
-    'rounded-sm border-2 border-white bg-grey px-2 text-white cursor-pointer',
+  paginationElement: "rounded-sm border-2 border-white bg-transparent px-2 text-white cursor-pointer",
+  currentPaginationElement: "rounded-sm border-2 border-white bg-grey px-2 text-white cursor-pointer",
+  nextButton: "rounded-sm border-2 border-white bg-transparent px-2 text-white cursor-pointer ml-2",
+  prevButton: "rounded-sm border-2 border-white bg-transparent px-2 text-white cursor-pointer mr-2",
 };
-export const Pagination = ({
-  element_number,
-  total_page,
-  current_page,
-  query,
-}: Props) => {
+export const Pagination = ({ element_number, total_page, current_page, query }: Props) => {
   const router = useRouter();
 
   let pageArray: number[] = [];
@@ -28,29 +24,29 @@ export const Pagination = ({
   if (total_page < element_number) {
     pageArray = Array.from({ length: total_page }, (_, i) => i + 1);
   } else if (Number(current_page + element_number / 2 + 1) >= total_page) {
-    pageArray = Array.from(
-      { length: element_number },
-      (_, i) => total_page - (element_number + 1) + i
-    );
+    pageArray = Array.from({ length: element_number }, (_, i) => total_page - (element_number - 1) + i);
   } else {
-    pageStart = Array.from(
-      { length: element_number / 2 },
-      (_, i) => current_page + i
-    );
-    pageEnd = Array.from(
-      { length: element_number / 2 },
-      (_, i) => total_page - 2 + i
-    ); //Math.max(current_page - Math.floor(element_number / 2), 1);
+    pageStart = Array.from({ length: element_number / 2 }, (_, i) => current_page + i);
+    pageEnd = Array.from({ length: element_number / 2 }, (_, i) => total_page - 2 + i); //Math.max(current_page - Math.floor(element_number / 2), 1);
   }
 
+  const [goTo, setGoTo] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    void router.push(`/search?query=${query}&page=${goTo}`);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGoTo(event.target.value);
+  };
+
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center ">
       {!(current_page == 1) && (
         <p
-          onClick={() =>
-            void router.push(`/search?query=${query}&page=${current_page - 1}`)
-          }
-          className={twClasses.paginationElement}
+          onClick={() => void router.push(`/search?query=${query}&page=${current_page - 1}`)}
+          className={twClasses.prevButton}
         >
           Prev
         </p>
@@ -61,15 +57,9 @@ export const Pagination = ({
           {pageArray.map((page) => {
             return (
               <p
-                className={
-                  page == current_page
-                    ? twClasses.currentPaginationElement
-                    : twClasses.paginationElement
-                }
+                className={page == current_page ? twClasses.currentPaginationElement : twClasses.paginationElement}
                 key={page}
-                onClick={() =>
-                  void router.push(`/search?query=${query}&page=${page}`)
-                }
+                onClick={() => void router.push(`/search?query=${query}&page=${page}`)}
               >
                 {page}
               </p>
@@ -81,15 +71,9 @@ export const Pagination = ({
         {pageStart.map((page) => {
           return (
             <p
-              className={
-                page == current_page
-                  ? twClasses.currentPaginationElement
-                  : twClasses.paginationElement
-              }
+              className={page == current_page ? twClasses.currentPaginationElement : twClasses.paginationElement}
               key={page}
-              onClick={() =>
-                void router.push(`/search?query=${query}&page=${page}`)
-              }
+              onClick={() => void router.push(`/search?query=${query}&page=${page}`)}
             >
               {page}
             </p>
@@ -97,22 +81,14 @@ export const Pagination = ({
         })}
       </div>
 
-      {current_page + element_number < total_page + 1 && (
-        <p className="text-white">................</p>
-      )}
+      {current_page + element_number < total_page + 1 && <p className="text-white">................</p>}
 
       <div className="flex items-center gap-2">
         {pageEnd.map((page) => {
           return (
             <p
-              onClick={() =>
-                void router.push(`/search?query=${query}&page=${page}`)
-              }
-              className={
-                page == current_page
-                  ? twClasses.currentPaginationElement
-                  : twClasses.paginationElement
-              }
+              onClick={() => void router.push(`/search?query=${query}&page=${page}`)}
+              className={page == current_page ? twClasses.currentPaginationElement : twClasses.paginationElement}
               key={page}
             >
               {page}
@@ -123,14 +99,20 @@ export const Pagination = ({
 
       {!(current_page == total_page) && (
         <p
-          onClick={() =>
-            void router.push(`/search?query=${query}&page=${current_page - 1}`)
-          }
-          className={twClasses.paginationElement}
+          onClick={() => void router.push(`/search?query=${query}&page=${current_page + 1}`)}
+          className={twClasses.nextButton}
         >
           Next
         </p>
       )}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          placeholder="Go To"
+          className="ml-2 w-20 appearance-none  border-2  bg-transparent px-2  text-base text-grey  placeholder-grey shadow-sm focus:outline-none"
+          onChange={handleChange}
+        />
+      </form>
     </div>
   );
 };
