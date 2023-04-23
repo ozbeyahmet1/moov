600eddb14a222cd58a8cbbe85795d5035210ffa3
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-23 03:09:13 +0300

    refactor: refactors movie section and changes name

--- src/interfaces/category.ts
+++
@@ -1,4 +0,0 @@
-export interface Category {
-  readonly id: number;
-  readonly name: string;
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
-import Image from 'next/image';
-import { Category } from '../interfaces/category';
+import { HeroSection } from '../components/sections/heroSection';
+import { PopularMoviesSection } from '../components/sections/popularMoviesSection';
+import { Genre } from '../interfaces/genre';
 import { Movie } from '../interfaces/movie';
 import {
   categoriesEndpoint,
@@ -10,26 +11,22 @@ import {
 } from '../utils';
 
 interface MoviesPageProps {
-  readonly trendingMovies: ReadonlyArray<Movie>;
   readonly topRatedMovies: ReadonlyArray<Movie>;
-  readonly netflixOriginals: ReadonlyArray<Movie>;
-  readonly categories: ReadonlyArray<Category>;
+  readonly popularMovies: ReadonlyArray<Movie>;
+  readonly categories: ReadonlyArray<Genre>;
 }
 
 export const getStaticProps: GetStaticProps<MoviesPageProps> = async () => {
   try {
-    const [trendingMovies, topRatedMovies, netflixOriginals, categories] =
-      await Promise.all([
-        getMoviesData(movieEndpoints.trending),
-        getMoviesData(movieEndpoints.trending),
-        getMoviesData(movieEndpoints.netflixOriginals),
-        getCategoriesData(categoriesEndpoint),
-      ]);
+    const [topRatedMovies, popularMovies, categories] = await Promise.all([
+      getMoviesData(movieEndpoints.topRated),
+      getMoviesData(movieEndpoints.popular),
+      getCategoriesData(categoriesEndpoint),
+    ]);
     return {
       props: {
-        trendingMovies,
         topRatedMovies,
-        netflixOriginals,
+        popularMovies,
         categories,
       },
       revalidate: 60 * 60 * 24, // Revalidate every 24 hours
@@ -42,32 +39,33 @@ export const getStaticProps: GetStaticProps<MoviesPageProps> = async () => {
   }
 };
 
-const MoviesPage = ({ trendingMovies, categories }: MoviesPageProps) => {
+const MoviesPage = ({
+  topRatedMovies,
+  popularMovies,
+  categories,
+}: MoviesPageProps) => {
   return (
-    <div>
-      <h1>Popular Movies</h1>
-      <div className="grid grid-cols-4">
-        {trendingMovies?.map((movie) => (
-          <div key={movie.id}>
-            {/* <h2>{movie.title}</h2>
-          <p>{movie.overview}</p> */}
-            <Image
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={`${movie.title} Poster`}
-              width={320}
-              height={400}
-            />
-          </div>
-        ))}
-      </div>
-      <div>
-        {categories.map((category) => {
-          return (
-            <h3 className="bg-red-500" key={category.id}>
-              {category.name}
-            </h3>
-          );
-        })}
+    <div className="pb-10">
+      <HeroSection
+        imageSource="https://res.cloudinary.com/droheqpxn/image/upload/v1681912683/movie-app/backgroundv3_fiu2ju.png"
+        font="mandalore text-8xl"
+        description="The travels of a lone bounty hunter in the outer reaches of the galaxy, far from the authority of the New Republic."
+        categories={[{ id: 18, name: 'Drama' }]}
+        movieName="Mandalorian"
+      />
+
+      <PopularMoviesSection movies={popularMovies} />
+
+      <div className="  bg-darkBlue px-0 py-5">
+        <div className="container grid grid-cols-10 gap-x-2 gap-y-2 p-0">
+          {categories.map((category) => {
+            return (
+              <h3 className="h-32 w-32 bg-blue" key={category.id}>
+                {category.name}
+              </h3>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
