62f317fbf5d2eee9a40ca32ba17ed35df1853fcf
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-24 08:35:32 +0300

    refactor: refactors search page interface

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -5,7 +5,7 @@ import { ParsedUrlQuery } from "querystring";
 import { MovieCard } from "../components/cards/movieCard/movieCard";
 import { Pagination } from "../components/ui/pagination/pagination";
 import { CardSize } from "../interfaces/movieCard";
-import { PropsWithMovieCards, getMoviesData, getTotalPages, getTotalResults, movieBySearchResult } from "../utils";
+import { PropsWithMovieCards, getMoviesData, getTotalPages, movieBySearchResult } from "../utils";
 
 interface SearchPageParams extends ParsedUrlQuery {
   readonly query: string;
@@ -13,7 +13,6 @@ interface SearchPageParams extends ParsedUrlQuery {
 }
 
 interface Props {
-  readonly total_results: number;
   readonly total_pages: number;
   readonly page: string;
 }
@@ -24,26 +23,24 @@ export const getServerSideProps: GetServerSideProps<PropsWithMovieCards, SearchP
   const query = context.query?.query as string;
   const page = context.query?.page as string;
   const endpoint = movieBySearchResult(query, page);
-  const total_results = await getTotalResults(endpoint);
   const total_pages = await getTotalPages(endpoint);
   const movies = await getMoviesData(endpoint);
 
   return {
     props: {
       movies,
-      total_results,
       total_pages,
       page,
     },
   };
 };
 
-const SearchPage = ({ movies, total_results, total_pages, page }: PropsWithMovieCards<Props>) => {
+const SearchPage = ({ movies, total_pages, page }: PropsWithMovieCards<Props>) => {
   const router = useRouter();
   const { query } = router.query;
   const [isXs] = useWindowSize();
   return (
-    <div className="container mt-24 flex flex-col  items-center px-0 pb-10 md:mt-32">
+    <div className="container mt-24 flex min-h-[60vh]  flex-col items-center px-0 pb-10 md:mt-32">
       {movies && movies.length > 0 ? (
         <>
           <div className="grid grid-cols-2 gap-x-2 gap-y-3 sm:grid-cols-3  md:grid-cols-3 md:gap-x-12 md:gap-y-12 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5">
@@ -61,7 +58,7 @@ const SearchPage = ({ movies, total_results, total_pages, page }: PropsWithMovie
           </div>
         </>
       ) : (
-        <p className="text-white">
+        <p className="p-4 text-white">
           We couldn&apos;t find a match for <b>&ldquo;{query}&ldquo;</b> . Please try another search
         </p>
       )}
