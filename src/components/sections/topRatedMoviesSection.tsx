899c8517cf3642923a8bd08b9eca488a47f12703
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-24 03:36:34 +0300

    feat: creates top rated movies section for homepage

---
+++ src/components/sections/topRatedMoviesSection.tsx
@@ -0,0 +1,16 @@
+import { CardSize } from "@/interfaces/movieCard";
+import { PropsWithMovieCards } from "@/utils";
+import { MovieCard } from "../cards/movieCardLg/movieCard";
+
+export const TopRatedSection = ({ movies }: PropsWithMovieCards) => {
+  return (
+    <section id="topRated" className=" container bg-blue px-0 py-10">
+      <h2 className="container mb-4 p-0 text-xl font-bold text-white">The bests of history</h2>
+      <div className="grid grid-cols-5 gap-y-20">
+        {movies?.map((movie, i) => (
+          <MovieCard key={i} index={i} movie={movie} size={CardSize.LG} />
+        ))}
+      </div>
+    </section>
+  );
+};
