9a78b892db21e47e77976b78043dd6118c2c2fd5
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-24 08:58:23 +0300

    refactor: adds ignore setings for storybook to prettier

--- src/components/tags/categoryTag/categoryTag.tsx
+++ src/components/tags/categoryTag/categoryTag.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export const CategoryTag = ({ category }: PropsWithMovieGenre) => {
   return (
     <Link href={`/categories/${category?.id ? category?.id : ""}`}>
-      <div className="rounded-md border-2 border-white px-3 py-1 text-sm font-semibold uppercase text-white">
+      <div className="w-fit rounded-md border-2 border-white px-3 py-1 text-sm font-semibold uppercase text-white">
         {category?.name}
       </div>
     </Link>
--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -6,15 +6,18 @@ const FavoritesPage = () => {
   const { movies } = useMovieContext();
   return (
     <div className="container mt-32 flex  min-h-[60vh]  flex-col items-center px-0 pb-10">
-      <div className="grid grid-cols-1 gap-x-12 gap-y-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5">
-        {movies && movies.length > 0 ? (
-          movies.map((movie, i) => {
+      {movies && movies.length > 0 ? (
+        <div className="grid grid-cols-1 gap-x-12 gap-y-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5">
+          {movies.map((movie, i) => {
             return <MovieCard key={i} index={i} movie={movie} size={CardSize.LG} />;
-          })
-        ) : (
-          <p className="text-white">There are no items in your favorites. Don&apos;t forget to click the heart button on the movie details page to add them.</p>
-        )}
-      </div>
+          })}
+        </div>
+      ) : (
+        <p className="p-2 text-white">
+          There are no items in your favorites. Don&apos;t forget to click the heart button on the movie details page to
+          add them.
+        </p>
+      )}
     </div>
   );
 };
