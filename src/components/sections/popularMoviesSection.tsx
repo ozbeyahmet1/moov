30f7acbe3b7f6dee1148ae37d216fd3fb2f628ba
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-24 03:38:44 +0300

    refactor: refactors popular movies section

--- src/components/sections/popularMoviesSection.tsx
+++ src/components/sections/popularMoviesSection.tsx
@@ -1,12 +1,13 @@
-import { PropsWithMovie } from "@/utils";
-import { MovieCard } from "../cards/movieCardLg/movieCardLg";
+import { CardSize } from "@/interfaces/movieCard";
+import { PropsWithMovieCards } from "@/utils";
+import { MovieCard } from "../cards/movieCardLg/movieCard";
 
-export const PopularMoviesSection = ({ movies }: PropsWithMovie) => {
+export const PopularMoviesSection = ({ movies }: PropsWithMovieCards) => {
   return (
-    <section id="features" className="hide-scroll-bar z-50 -mt-32 flex overflow-x-scroll bg-blue">
-      <div className="flex flex-nowrap gap-3 px-12 py-8" id="my-scrollable-div">
+    <section id="features" className="hide-scroll-bar z-50 -mt-32 flex overflow-x-scroll bg-blue pb-16 pt-0">
+      <div className="flex flex-nowrap gap-3 gap-y-6 px-12 py-12" id="my-scrollable-div">
         {movies?.map((movie, i) => (
-          <MovieCard key={i} index={i} movie={movie} />
+          <MovieCard index={i} movie={movie} size={CardSize.LG} key={i} />
         ))}
       </div>
     </section>
