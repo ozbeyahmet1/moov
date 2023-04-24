26be896551eddec5c2d77f4f6821ed3921b61f47
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-24 03:32:23 +0300

    feat: creates category detail page,category section and category card

--- src/components/sections/categoriesSection.tsx
+++ src/components/sections/categoriesSection.tsx
@@ -1,18 +1,15 @@
-import { Genre } from "@/interfaces/genre";
+import { PropsWithMovieGenres } from "@/utils";
 import { CategoryCard } from "../cards/categoryCard/categoryCard";
 
-interface Props {
-  readonly categories: ReadonlyArray<Genre>;
-}
-
-export const CategoriesSection = ({ categories }: Props) => {
+export const CategoriesSection = ({ categories }: PropsWithMovieGenres) => {
   return (
     <section className="  bg-darkBlue px-0 py-10 ">
       <h2 className="container mb-4 p-0 text-xl font-bold text-white">Watch by Categories</h2>
-      <div className="container grid grid-cols-9 gap-x-2 gap-y-4 p-0">
-        {categories.slice(0, 18).map((category) => {
-          return <CategoryCard category={category} key={category.id} />;
-        })}
+      <div className="container grid grid-cols-9 gap-x-2 gap-y-6 p-0">
+        {categories &&
+          categories.slice(0, 18).map((category) => {
+            return <CategoryCard category={category} key={category.id} />;
+          })}
       </div>
     </section>
   );
